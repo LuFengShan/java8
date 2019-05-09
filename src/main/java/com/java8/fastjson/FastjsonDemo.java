89446e171725e6d5274fc552d8c28bac6189a895
LuFengShan <sun_guang_xu@163.com>
2019-05-09 15:03:47 +0800

    新增fastjson的包
    新加fastjson的测试用例的说明

--- src/main/java/com/java8/fastjson/FastjsonDemo.java
+++ src/main/java/com/java8/fastjson/FastjsonDemo.java
@@ -1,32 +1,68 @@
 package com.java8.fastjson;
 
-public class FastjsonDemo {
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.logging.Logger;
+import org.junit.platform.commons.logging.LoggerFactory;
 
-  //  public static void main(String[] args) {
-  //    Person p = new Person();
-  //    p.setFirstName("san");
-  //    p.setLastName("zhang");
-  //    Person p1 = new Person();
-  //    p1.setFirstName("si");
-  //    p1.setLastName("li");
-  //
-  //    // 直接把对象序列化成json格式
-  //    String jsonString = JSON.toJSONString(p); // {"firstName":"san","lastName":"zhang"}
-  //    System.out.println(jsonString);
-  //    // 把对象转化为json格式 ，但是没有key值，只存value
-  //    String string_BeanToArray = JSON.toJSONString(p, SerializerFeature.BeanToArray); //
-  // ["san","zhang"]
-  //    System.out.println(string_BeanToArray);
-  //
-  //    PersonList personList = new PersonList();
-  //    personList.setCone(1001);
-  //    List<Person> list = new ArrayList<>();
-  //    list.add(p);
-  //    list.add(p1);
-  //    personList.setList(list);
-  //    String jsonString2 = JSON.toJSONString(personList);
-  //    System.out.println(jsonString2);
-  //
-  //  }
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * fastjson示例
+ */
+public class FastjsonDemo {
+    private static final Logger logger = LoggerFactory.getLogger(FastjsonDemo.class);
+    private Person p;
+    private Person p1;
+
+    @BeforeEach
+    public void initPerson() {
+        p = new Person();
+        p.setFirstName("san");
+        p.setLastName("zhang");
+        p1 = new Person();
+        p1.setFirstName("si");
+        p1.setLastName("li");
+    }
+
+    /**
+     * 直接把对象序列化成json格式
+     */
+    @Test
+    public void testObjectMapJson() {
+        logger.info(() -> JSON.toJSONString(p));
+        logger.info(() -> JSON.toJSONString(p1));
+    }
+
+    /**
+     * 把对象转化为json格式 ，但是没有key值，只存value
+     */
+    @Test
+    public void testObjectMapJsonWithSerializerFeature() {
+        String string = JSON.toJSONString(p, SerializerFeature.BeanToArray);
+        logger.info(() -> string); // ["san", "zhang"]
+        if (string.startsWith("["))
+            logger.info(() -> "true");
+    }
+
+    /**
+     * 一对多的一个转换
+     */
+    @Test
+    public void testArraysMapJson() {
+        PersonList personList = new PersonList();
+
+        List<Person> list = new ArrayList<>();
+        list.add(p);
+        list.add(p1);
+
+        personList.setCone(1001);
+        personList.setList(list);
+
+        logger.info(() -> JSON.toJSONString(personList)); // {"cone":1001,"list":[["san","zhang"],["si","li"]]}
+    }
 
 }
--- src/main/java/com/java8/fastjson/Person.java
+++ src/main/java/com/java8/fastjson/Person.java
@@ -1,38 +1,50 @@
 package com.java8.fastjson;
 
-// @JSONType(serialzeFeatures = SerializerFeature.BeanToArray,
-// parseFeatures = Feature.SupportArrayToBean)
+import com.alibaba.fastjson.serializer.SerializerFeature;
+
+/**
+ * <p>
+ * {@link com.alibaba.fastjson.annotation.JSONType} : 可以在对象上面直接放，这样出来的json直接返回的是字符串。
+ *
+ * <p>
+ * {@link SerializerFeature#BeanToArray}:普通模式下，JavaBean映射成json object，BeanToArray模式映射为json array。
+ * 也就是普通模式下带上对象的属性值，如{"firstName":"san","lastName":"zhang"}；
+ * BeanToArray模式下是只显示kv中的v值，返回的是一个字符串，如["san","zhang"]
+ */
+//@JSONType(serialzeFeatures = SerializerFeature.BeanToArray,
+//        parseFeatures = Feature.SupportArrayToBean)
 public class Person {
-  String firstName;
-  String lastName;
+    String firstName;
+    String lastName;
 
-  public String getFirstName() {
-    return firstName;
-  }
+    public String getFirstName() {
+        return firstName;
+    }
 
-  public void setFirstName(String firstName) {
-    this.firstName = firstName;
-  }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
-  public String getLastName() {
-    return lastName;
-  }
+    public String getLastName() {
+        return lastName;
+    }
 
-  public void setLastName(String lastName) {
-    this.lastName = lastName;
-  }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
-  Person() {}
+    Person() {
+    }
 
-  Person(String firstName, String lastName) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-  }
+    Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 
-  @Override
-  public String toString() {
-    return lastName + firstName;
-  }
+    @Override
+    public String toString() {
+        return lastName + firstName;
+    }
 
 
 }
