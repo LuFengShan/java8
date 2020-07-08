6f921a69c6bd50b6755f32340c3295620d2abde0
sunguangxu <sun_guang_xu@163.com>
2020-07-08 17:50:39 +0800

    增加测试用例

--- src/main/java/com/java8/meiju/EnumTest.java
+++ src/main/java/com/java8/meiju/EnumTest.java
@@ -74,6 +74,14 @@ public class EnumTest {
 		System.out.println(colorInterface);
 	}
 	
+	@Test
+	public void single() {
+		Singleton instance = Singleton.getInstance();
+		Singleton instance1 = Singleton.getInstance();
+		instance.sayHi();
+		System.out.println(instance == instance1);
+	}
+	
 }
 
 interface ColorInterface {
@@ -150,5 +158,36 @@ enum ErrorCodeEnum {
 			}
 		}
 		return UNKNOWN_ERROR;
+	}
+}
+
+class Singleton {
+	//  枚举类型是线程安全的，并且只会装载一次
+	private enum SingletonEnum {
+		INSTANCE;
+		//  声明单例对象
+		private final Singleton instance;
+		
+		//  实例化
+		SingletonEnum() {
+			instance = new Singleton();
+		}
+		
+		private Singleton getInstance() {
+			return instance;
+		}
+	}
+	
+	//  获取实例（单例对象）
+	public static Singleton getInstance() {
+		return SingletonEnum.INSTANCE.getInstance();
+	}
+	
+	private Singleton() {
+	}
+	
+	//  类方法
+	public void sayHi() {
+		System.out.println("Hi,Java.");
 	}
 }
